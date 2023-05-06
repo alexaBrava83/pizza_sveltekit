9399a9d15ae1b8758f70812706f3c0b173111800
AlexaBrava83 <alexa.brava83@gmail.com>
2023-05-06 16:57:54 +0300

    Start new poject

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import "../style/app.scss";
+</script>
+
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="img/logo.png" alt="logo" class="icon" />
+            <div class="info">
+                <h3 class="digit-title">SVELTE PIZZA</h3>
+                <p class="description">найсмачніша піца у всесвіті</p>
+            </div>
+        </div>
+        <div class="button">
+            <button class="btn orange">button</button>
+        </div>
+    </div>
+</header>
+
+<slot></slot>
+
+<footer>
+    <div class="container">Made by Sasha Pysaruk</div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+
+
+<section id="menu">
+    <div class="container">
+        <div class="pizza-block">
+            <img src="img/pizza_1.png" alt="pizza_1">
+            <div class="name">Чізбургер піца</div>
+            <div class="parametrs">
+                <div class="block">
+                    <div class="parametr select">Тонка</div>
+                    <div class="parametr">Традиційна</div>
+                </div>
+                <div class="block">
+                    <div class="parametr select">26см</div>
+                    <div class="parametr">30см</div>
+                    <div class="parametr">40см</div>
+                </div>
+            </div>
+            <div class="price">
+                <div class="amount">від $326</div>
+                <button class="btn orange">Додати</button>
+            </div>
+        </div>
+    </div>
+</section>
+
